e5ae5fa485c305b40e448ee7f716b8711833f717
equewww <dasha200641@gmail.com>
2025-02-17 09:55:45 +0300

    Add files via upload

---
+++ 2222.py
@@ -0,0 +1,19 @@
+"""Минимальный элемент заданного одномерного массива увеличить в 2 раза"""
+file= open('C:/Users/m2402714/Desktop/m.txt')
+s = file.readlines()
+file.close()
+print(s)
+m=[]
+for i in s:
+    if i != '':
+        print(i.strip())
+        for ii in i.split():
+            m.append(int(ii))
+min_value = min(m)
+min_index = m.index(min_value)
+m[min_index] = 2 * min_value
+file= open('C:/Users/m2402714/Desktop/m.txt', 'w')
+for k in m:
+    file.write(str(k) + '\n')
+file.close()
+print('Ответ:', 2 * min_value)
